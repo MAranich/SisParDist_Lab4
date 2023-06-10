36ca101211d9990daf9bc61c95c33c55af8047bd
MAranich <114926980+MAranich@users.noreply.github.com>
2023-06-10 20:52:13 +0200

    update 6

--- lab4_T2G14/1_triangulation/delaunay.cu
+++ lab4_T2G14/1_triangulation/delaunay.cu
@@ -130,6 +130,14 @@ void init_points(struct Point* points, int num_points, int width, int height) {
     }
 }
 
+
+/////////////////////////////////////////////
+///
+///         CUDA part
+///
+/////////////////////////////////////////////
+
+
 /*Kernel function: to be executed on the device and launched from the host*/
 __global__ void count_close_points_CUDA(struct Point* points) {
 
@@ -176,7 +184,7 @@ __global__ void delaunay_triangulation_CUDA(struct Point* points, int num_points
     int j = (n_totalIter / num_points) % num_points;
     int k = n_totalIter % num_points; 
 
-    if( !( i < j && j < k ) ) return; //if the conditions are NOT met, return
+    if( !( i < j && j < k ) ) return; //if the conditions are NOT met, en thread
     //calculate triangle
     struct Triangle triangle_new;
 
@@ -225,18 +233,18 @@ void delaunay_triangulation_gpu(struct Point* points, int num_points, struct Tri
     delaunay_triangulation_CUDA<<<totalIters>>>(cudaPoints, num_points, cudaTriangle, nt); 
     //Syncronitzation (?)
 
-    cudaMemcpy(d_nt, &h_nt, sizeof(int), cudaMemcpyHostToDevice); //retrive nt (?)
+    cudaMemcpy(d_nt, &h_nt, sizeof(int), cudaMemcpyDeviceToHost); //retrive nt (?)
 
 
     //no need to retrive points since they are not affected
-    cudaMemcpy(cudaTriangle, triangles, sizeof(struct Triangle) * h_nt, cudaMemcpyHostToDevice); //retrive only the necessary triangles
+    cudaMemcpy(triangles, cudaTriangle, sizeof(struct Triangle) * h_nt, cudaMemcpyDeviceToHost); //retrive only the necessary triangles
 
     *num_triangles = h_nt; //save the value of 
     cudaFree(cudaPoints);
     cudaFree(cudaTriangle);
 
 
-    //Display some info and print some of the triangles
+    //Display some info and print some of the triangles //delete later
     if(0 <= h_nt){
         int rnd = 5; //bad pseudoransom number generator
         printf("delaunay_triangulation_gpu finalized. Created %d triangles (neg number = error) \n", *num_triangles)
@@ -252,19 +260,98 @@ void delaunay_triangulation_gpu(struct Point* points, int num_points, struct Tri
 }
 
 
-__global__ void save_triangulation_points_CUDA(struct Point* points) {
+__global__ void save_triangulation_points_CUDA(struct Point* points, int num_points, struct Triangle* triangles, int* num_triangles, double* image, int width, int height) {
     
+    int i = threadIdx.x + blockIdx.x * blockDim.x; //get position of pixel
+    int j = threadIdx.y + blockIdx.y * blockDim.y; 
+
+    //declare vars
+    int inside = 0;
+    struct Point pixel, *point;
+    struct Triangle* tr = NULL; 
+    double alpha, beta, gamma;
+
+    pixel.x = (double)i; 
+    pixel.y = (double)j; 
+    pixel.value = 0;
+
+    image[pixel(i, j, width)] = -1; //set deafult value
+
+
+    for(int k = 0; k < num_triangles; k++){             //recorre todos los triangulos
+        tr = &triangles[k]; 
+        barycentric_coordinates(tr, &pixel, &alpha, &beta, &gamma); 
+        if(0 < alpha && 0 < beta && 0 < gamma){ //if inside triangle
+            image[pixel(i, j, width)] = tr->p1.value * alpha + tr->p2.value * beta + tr->p3.value * gamma;   //sets new value
+            break; // podria ser un return
+        }
+    }
+
+
+
 }
+
+__global__ void save_BlackBox_CUDA(struct Point* points, int num_points, double* image, int width, int height) {
+
+    int k = threadIdx.x + blockIdx.x * blockDim.x; 
+
+    int _x = points[k].x; //get coord of point
+    int _y = points[k].y; 
+    
+    int radius = 2; // Total size = (2 * radius + 1)^2
+
+    //square of size 5
+
+    for(int i = _x - radius;  i <= _x + radius; i++) { //in a box
+        for(int j = _y - radius; j <= _y + radius; j++) {
+            if(0 <= i && 0 <= j && i < width && j < height) { //if possible
+                image[(pixel(i, j, width))] = 101.0; //draw black pixel
+            }
+        }
+    }
+
+}
+
 
 /*Wraper function to launch the CUDA kernel to compute delaunay triangulation. 
 Remember to store an image of int's between 0 and 100, where points store 101, and empty areas -1, and points inside triangle the average of value */
 void save_triangulation_image_gpu(struct Point* points, int num_points, struct Triangle* triangles, int num_triangles, int width, int height) {
-    //create structures
-    double* image = (double *) malloc(sizeof(double)*width*height); 
-
-
-
     
+    //create structures
+    int size = width * height;
+    double* image = (double*) malloc(sizeof(double)*size);
+
+    //copy points to gpu
+    struct Point* cudaPoints; //ptr GPU
+    cudaMalloc(&cudaPoints, sizeof(struct Point) * num_points); //allocate space
+    cudaMemcpy(cudaPoints, points, sizeof(struct Point) * num_points, cudaMemcpyHostToDevice); //data transfer
+
+    //copy triangles to gpu
+    struct Triangle* cudaTriangles; //ptr GPU
+    cudaMalloc(&cudaTriangles, sizeof(struct Triangle) * num_triangles); //allocate space
+    cudaMemcpy(cudaTriangles, triangles, sizeof(struct Triangle) * num_triangles, cudaMemcpyHostToDevice); //data transfer
+
+    double* cudaImage; //ptr GPU
+    cudaMalloc(&cudaImage, sizeof(double) * size); //allocate space
+    //data created in gpu
+
+    //usamos un thread en la gpu por pixel
+    save_triangulation_points_CUDA<<<width, height>>>(cudaPoints, num_points, cudaTriangles, num_triangles, cudaImage, width, height);                                
+
+    cudaFree(cudaTriangles); //not needed anymore
+
+    //wait for next kernel
+    //keep image in gpu, no need to move it
+    //also keep points there
+
+    save_BlackBox_CUDA<<<num_points>>>(cudaPoints, num_points, cudaImage, width, height); 
+
+    cudaMemcpy(image, cudaImage, sizeof(double) * size, cudaMemcpyDeviceToHost); //retrive image
+
+    cudaFree(cudaPoints); 
+    cudaFree(cudaImage); 
+
+
     //write image
     save_image("image.txt", width, height, image);
 
